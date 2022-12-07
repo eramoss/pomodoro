d3ad36260c2bb51777dc85bac42750df716b3d70
eramoss <edulramos@outlook.com>
2022-12-07 18:13:49 -0300

    set styles globals with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+
+  darkMode: ['class', '[data-mode="isSelected"]'],
+
   theme: {
-    extend: {},
+   
+    
+    extend: {
+      colors: {
+        'midnight': '#1e2240',
+        'midnight-dark': '#141a31',
+        'orange-datail': '#f56e6f',
+      },
+      boxShadow: {
+        'light': '-11px -11px 44px #1c2544',
+        'dark': '11px 11px 44px #0c0f1c',
+      }
+      
+     
+    },
   },
   plugins: [],
 }
