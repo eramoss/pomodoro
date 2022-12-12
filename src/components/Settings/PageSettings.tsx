f6e76025f70a599456f6449e6c6f65dbb250d8f5
eramoss <edulramos@outlook.com>
2022-12-12 10:48:58 -0300

    creating design base to settings modal

--- src/components/Settings/PageSettings.tsx
+++ src/components/Settings/PageSettings.tsx
@@ -1,16 +1,19 @@
 import { Switcher } from "../Switcher";
+import {CgSun,CgEditContrast} from 'react-icons/cg'
 
 export const PageSettings = () => {
   const handleClick = () => {
     console.log("aé");
   };
 
+
   return (
-    <div className=" w-full h-full p-4 flex items-center justify-center gap-5  overflow-hidden">
-      <div className="flex flex-col  justify-center items-center gap-3">
-        <Switcher label="jurema" functionSwitch={handleClick} />
-        <Switcher label="jurandir" functionSwitch={handleClick} />
+    <div className=" w-full h-full   py-16 flex flex-col items-center justify-start gap-5  overflow-hidden">
+      <div className="w-full flex justify-center items-start flex-col mt-10">
+        <Switcher  icon={<CgSun />} label= "Light Mode" functionSwitch={handleClick} />
+        <Switcher icon={<CgEditContrast/>} label="high Contrast" functionSwitch={handleClick} />
       </div>
+
     </div>
   );
 };
--- src/components/Switcher.tsx
+++ src/components/Switcher.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 interface SwitcherProps {
   label: string;
+  icon?: React.ReactNode;
   functionSwitch : () => void
 }
 
 
-export const Switcher = ({functionSwitch,label}:SwitcherProps) => {
+export const Switcher = ({functionSwitch,label,icon}:SwitcherProps) => {
 
   const [switch1, setSwitch1] = useState('bg-gray-400')
   var positionSwitch = 'translate-x-0'
@@ -26,13 +27,23 @@ export const Switcher = ({functionSwitch,label}:SwitcherProps) => {
 
 
   return (
-    <div className="flex items-center gap-2 ">
-      <label htmlFor="button1">{label}</label>
-        <button onClick={functionSwitch }>
-          <button onClick={handleClick} className={`rounded-full w-14 h-8 px-[1px] border-[3px] border-black focus:border-[3.3px] flex items-center  ${switch1} `} id="button1">
-          <div className={`rounded-full h-6 w-6 ${positionSwitch} bg-slate-50 transition`} ></div>
-        </button>
-      </button>
+    <div className="flex items-center w-full justify-between bg-white p-3 border-b border-gray-300 ">
+      <div className="flex items-center gap-4">
+        <div className="text-3xl">
+          {icon}
+        </div>
+        
+        <label  className="text-md tracking-wider" >{label}</label>
+      </div>
+      
+      <span>
+        <div onClick={functionSwitch }>
+          <button onClick={handleClick} className={`rounded-full w-14 h-8 px-[1px] border-[3px] border-black focus:border-[3.3px] flex items-center  ${switch1} `}>
+            <div className={`rounded-full h-6 w-6 ${positionSwitch} bg-slate-50 transition`} ></div>
+          </button>
+        </div>
+      </span>
+        
     </div>
   )
 }
